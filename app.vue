6a6b6500c4bf6d9f9ec1c9dced982c94d3a96ffb
Martasantenbo <martasan.donuts@gmail.com>
2022-11-13 18:30:09 +0100

    Initial commit

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Macondo+Swash+Caps&family=Montserrat:wght@200;400&display=swap');
+</style>
